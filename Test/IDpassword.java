ddd3015a0457301b474d8aa6f2496a6ba6f29771
JG_Spectre <167601605+JG-Spectre@users.noreply.github.com>
2026-01-17 18:02:58 +0900

    Add files via upload

--- Test/IDpassword.java
+++ Test/IDpassword.java
@@ -3,22 +3,21 @@ package Test;
 import java.util.Scanner;
 
 public class IDpassword {
+	
 	public static void main(String[] args) {
 		String[] ids = new String[100];
 		String[] pws = new String[100];
-		int count=0;
-		Scanner sc=new Scanner(System.in);
-		while(true)
-		{
+		int count = 0;
+		Scanner sc = new Scanner(System.in);
+		while (true) {
 			System.out.print("명령 입력\nlogin << 로그인\nsignup << 회원가입\nexit << 프로그램 종료\n>>");
-			String chose=sc.nextLine();
-			switch(chose)
-			{
+			String chose = sc.nextLine();
+			switch (chose) {
 			case "login":
-				login(ids,pws,count);
+				login(ids, pws, count);
 				break;
 			case "signup":
-				signup(ids,pws,count);
+				signup(ids, pws, count);
 				break;
 			case "exit":
 				System.out.println("프로그램 종료");
@@ -27,26 +26,39 @@ public class IDpassword {
 		}
 	}
 
-	private static void signup(String[] ids,String[] pws,int count) {
+	private static void signup(String[] ids, String[] pws, int count) {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		while(true)
-		{
+		String inputid;
+		Scanner sc = new Scanner(System.in);
+		while (true) {
 			System.out.print("아이디 입력\n(뒤로 가려면 exit 입력)\n>>");
-			String inputid=sc.nextLine();
-			if(count>0)
-				for(int i=0;i<count;i++)
-				{
-					if(inputid.equals(ids[count]))
-						System.out.println("이미 존재하는 아이디입니다.\n");
-					
+			inputid = sc.nextLine();
+			boolean existing = false;
+			if (inputid.equals("exit"))
+				return;
+			for (int i = 0; i < count; i++)
+				if (inputid.equals(ids[count])) {
+					existing = true;
+					break;
 				}
+			if (existing == true) {
+				System.out.println("이미 존재하는 아이디입니다.\n");
+				continue;
+			}
+			break;
 		}
-		
-			
+		System.out.print("비밀번호 입력(뒤로 가려면 back 입력)\n>>");
+		String inputpw = sc.nextLine();
+		if (inputpw.equals("back"))
+			return;
+		count+=1;
+		ids[count] = inputid;
+		pws[count] = inputpw;
+		System.out.println("회원가입이 완료되었습니다.");
+
 	}
 
-	private static void login(String[] ids,String[] pws,int count) {
+	private static void login(String[] ids, String[] pws, int count) {
 		// TODO Auto-generated method stub
 		int accountnumber;
 	}
