7215c70f8d85bb96cc585f96e0801c8b0a6d265d
JG_Spectre <167601605+JG-Spectre@users.noreply.github.com>
2026-07-11 14:02:47 +0900

    Add files via upload

--- 용사키우기/용사.java
+++ 용사키우기/용사.java
@@ -2,25 +2,135 @@ package 용사키우기;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class 용사 {
-	private String name; private int level; private int hp; private int maxhp; private int attack; private int defense; private int stage;
+	private String name; private int level; private int hp; private int maxhp; private int shield; private int maxshield; private int attack; private int defense; private int stage; private int coin;
 	private final List<아이템> inventory = new ArrayList<>();
-	public 용사(String name,int maxHp, int attack,int defense) { this.name = name; this.level = 1; this.maxhp = maxHp; this.hp = maxHp; this.attack = attack; this.defense = defense; }
+	public 용사(String name,int maxHp, int shield, int attack,int defense) { this.name = name; this.level = 1; this.maxhp = maxHp; this.hp = maxHp; this.shield=shield; this.attack = attack; this.defense = defense; }
 	public void 아이템추가(아이템 item) { inventory.add(item); }
-	public void 아이템사용(아이템 item)
-	{
+	public void 아이템사용(아이템 item) {
 		
 	}
 	public void 데미지받기(int damage) throws 게임오버Exception { hp -= damage; }
-	public void 상태출력() { System.out.printf("[%s] Lv.%d HP:%d/%d ATK:%d DEF:%d%n",name,level,hp,maxhp,attack,defense); }
+	public void 상태출력() { System.out.printf("[%s] Lv.%d HP:%d/%d SHIELD:%d ATK:%d DEF:%d%n",name,level,hp,maxhp,maxshield,attack,defense); }
 	public String 이름반환() { return name; }
 	public int 체력반환() { return hp; }
 	public int 단계반환() { return stage; }
+	public int 코인반환() { return coin; }
 	public List<아이템> 인벤토리반환() { return inventory; }
+	
 	public void 토벌() {
+		Scanner sc = new Scanner(System.in);
 		System.out.println();
-		System.out.println("무엇이든 천천히 하는 것이 중요하다.\n토벌을 시작할까?");
+		System.out.print("무엇이든 천천히 하는 것이 중요하다.\n토벌을 시작할까? (현재 토벌 지역 : ");
+		switch(단계반환()) {
+		case 1:System.out.print("고블린 동굴");break;
+		case 2:System.out.print("울창한 숲");break;
+		case 3:System.out.print("산적 전초기지");break;
+		case 4:System.out.print("파괴된 도시");break;
+		case 5:System.out.print("마법이 깃든 숲");break;
+		case 6:System.out.print("마계 초원");break;
+		case 7:System.out.print("마왕성");break;
+		}
+		System.out.print(")\n1. 가자!\n2. 이전 지역 소탕 (이전 소탕 지역 : ");
+		switch(단계반환()) {
+		case 1:System.out.print("없음");break;
+		case 2:System.out.print("고블린 동굴");break;
+		case 3:System.out.print("울창한 숲");break;
+		case 4:System.out.print("산적 전초기지");break;
+		case 5:System.out.print("파괴된 도시");break;
+		case 6:System.out.print("마법이 깃든 숲");break;
+		case 7:System.out.print("마계 초원");break;
+		}
+		System.out.print(")\n>>");
+		int choice = sc.nextInt();
+		switch(choice) {
+			case 1:	{
+				switch(단계반환()) {
+				case 1:System.out.print("고블린 동굴");break;
+				case 2:System.out.print("울창한 숲");break;
+				case 3:System.out.print("산적 전초기지");break;
+				case 4:System.out.print("파괴된 도시");break;
+				case 5:System.out.print("마법이 깃든 숲");break;
+				case 6:System.out.print("마계 초원");break;
+				case 7:System.out.print("마왕성");break;
+				}
+			}
+		}
+	}
+	private void 토벌_고블린동굴() {
 		
+	}
+	private void 토벌_울창한숲() {
+		
+	}
+	private void 토벌_산적전초기지() {
+		
+	}
+	private void 토벌_파괴된도시() {
+	
+	}
+	private void 토벌_마법이깃든숲() {
+	
+	}
+	private void 토벌_마계초원() {
+	
+	}
+	private void 토벌_마왕성() {
+	
+	}
+	public void 상점() {
+		System.out.println("\n상점이다. 어떤걸 구매해볼까? (잔액 : "+코인반환()+"코인)");
+		
+	}
+	public void 단련() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\n단련장이다. 어떤걸 단련해볼까? (잔액 : "+코인반환()+"코인)\n1. 체력 단련\n2.공격력 단련\n3. 방어력 단련\n(모든 단련은 100코인을 소모한다.)");
+		int choice = sc.nextInt();
+		switch(choice) {
+			case 1: {
+				if (코인반환() >= 100)
+				{
+					maxhp+=5;
+					coin-=100;
+					System.out.println("체력 단련 완료\n최대체력이 5 증가했다.\n");
+				}
+				else
+					System.out.println("단련하기엔 잔액이 부족하다.");
+				break;
+			}
+			case 2: {
+				if (코인반환() >= 100)
+				{
+					attack+=1;
+					coin-=100;
+					System.out.println("공격력 단련 완료\n공격력이 1 증가했다.\n");
+				}
+				else
+					System.out.println("단련하기엔 잔액이 부족하다.");
+				break;
+			}
+			case 3: {
+				if (코인반환() >= 100)
+				{
+					defense+=1;
+					coin-=100;
+					System.out.println("방어력 단련 완료\n방어력이 1 증가했다.\n");
+				}
+				else
+					System.out.println("단련하기엔 잔액이 부족하다.");
+				break;
+			}
+		}
+	}
+	public void 인벤토리() {
+		인벤토리반환();
+		//아이템 사용 및 착용 여부
+		
+	}
+	public void 프로필() {
+		System.out.println("\n이게 나다.");
+		상태출력();
 	}
 }
--- 용사키우기/용사키우기.java
+++ 용사키우기/용사키우기.java
@@ -6,26 +6,35 @@ public class 용사키우기 {
 	public static void main(String[] args) {
 		용사키우기 starter = new 용사키우기();
 		starter.start();
+		System.out.println("\n푸딩은 그냥 다시 사오면 된다. 그냥 집에서 쉬자.");
 	}
 	void start()
 	{
-		용사 hero = new 용사("용사",100,10,5);
 		Scanner sc = new Scanner(System.in);
+		String heroname;
+		System.out.println("용사의 이름은?");
+		heroname = sc.nextLine();
+		용사 hero = new 용사(heroname,100,0,10,5);
 		
 //		try
 //		{
 			introduction(hero.이름반환());
 			hero.상태출력();
-			while (true)
-			{
+			
+			while (true) {
 				System.out.println();
 				System.out.print("이제 뭘 해야될까?\n1. 토벌\n2. 상점\n3. 단련\n4. 인벤토리\n5. 프로필\n6. 포기\n>>");
 				int main_choice = sc.nextInt();
-				switch(main_choice)
-				{
-				case 1:hero.토벌();break;
+				switch(main_choice) {
+					case 1:hero.토벌();break;
+					case 2:hero.상점();break;
+					case 3:hero.단련();break;
+					case 4:hero.인벤토리();break;
+					case 5:hero.프로필();break;
+					case 6:return;
 				}
 			}
+			
 //		}
 //		catch(게임오버Exception e)
 //		{
@@ -39,13 +48,16 @@ public class 용사키우기 {
 	}
 	void introduction(String heroname)
 	{
+		Scanner sc = new Scanner(System.in);
 		System.out.println("때는 마계가 지구와 공존하던 시절.");
 		System.out.println("클레멘스의 일반 시민 "+heroname+".");
 		System.out.println("배가 고파서 전에 사둔 푸딩을 먹으려 하는데...");
 		System.out.println("아니글쎄 푸딩이 사라진 것이다!");
 		System.out.println("그리고 놓여있는 쪽지 한장.");
 		System.out.println("\"잘 먹고감 ㅋ\" -마왕");
-		System.out.println("그렇게 용사는 모험을 시작하게 된다...");
+		System.out.println("그렇게 용사는 모험을 시작하게 된다...\n");
+		System.out.println("넘어가려면 아무거나 입력하고 엔터");
+		sc.next();
 		System.out.println();
 	}
 }
