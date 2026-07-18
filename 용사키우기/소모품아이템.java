e85af66074a0010e80d4bd985e628f180e230bf8
JG_Spectre <167601605+JG-Spectre@users.noreply.github.com>
2026-07-18 14:01:04 +0900

    Add files via upload

--- 용사키우기/소모품아이템.java
+++ 용사키우기/소모품아이템.java
@@ -1,5 +1,8 @@
 package 용사키우기;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class 소모품아이템 extends 아이템 {
 	private final int healAmount;
 	public 소모품아이템(아이템아이디 id, int healAmount)
@@ -10,7 +13,15 @@ public class 소모품아이템 extends 아이템 {
 	@Override
 	public void 사용(용사 hero)
 	{
-		
+		final List<아이템> inventory = hero.인벤토리반환();
+		for (int i = 0; i < inventory.size();i++)
+		{
+			아이템 myitem = inventory.get(i);
+			
+			if(myitem.아이템반환() == this.id) {
+				hero.
+			}
+		}
 	}
 	public int 회복량반환()
 	{
--- 용사키우기/용사.java
+++ 용사키우기/용사.java
@@ -43,38 +43,49 @@ public class 용사 {
 		case 6:System.out.print("마법이 깃든 숲");break;
 		case 7:System.out.print("마계 초원");break;
 		}
-		System.out.print(")\n>>");
+		System.out.print(")\n3. 뒤로\n>>");
 		int choice = sc.nextInt();
 		switch(choice) {
 			case 1:	{
 				switch(단계반환()) {
-				case 1:System.out.print("고블린 동굴");break;
-				case 2:System.out.print("울창한 숲");break;
-				case 3:System.out.print("산적 전초기지");break;
-				case 4:System.out.print("파괴된 도시");break;
-				case 5:System.out.print("마법이 깃든 숲");break;
-				case 6:System.out.print("마계 초원");break;
-				case 7:System.out.print("마왕성");break;
+				case 1:토벌_고블린동굴(true);break;
+				case 2:토벌_울창한숲(true);break;
+				case 3:토벌_산적전초기지(true);break;
+				case 4:토벌_파괴된도시(true);break;
+				case 5:토벌_마법이깃든숲(true);break;
+				case 6:토벌_마계초원(true);break;
+				case 7:토벌_마왕성();break;
+				}
+			}
+			case 2:	{
+				switch(단계반환()) {
+				case 1:System.out.println("이전 단계? 내가 뭐, 이 마을이라도 토벌해야되나?");break;
+				case 2:토벌_고블린동굴(false);break;
+				case 3:토벌_울창한숲(false);break;
+				case 4:토벌_산적전초기지(false);break;
+				case 5:토벌_파괴된도시(false);break;
+				case 6:토벌_마법이깃든숲(false);break;
+				case 7:토벌_마계초원(false);break;
 				}
 			}
 		}
 	}
-	private void 토벌_고블린동굴() {
+	private void 토벌_고블린동굴(boolean isNew) {
 		
 	}
-	private void 토벌_울창한숲() {
+	private void 토벌_울창한숲(boolean isNew) {
 		
 	}
-	private void 토벌_산적전초기지() {
+	private void 토벌_산적전초기지(boolean isNew) {
 		
 	}
-	private void 토벌_파괴된도시() {
+	private void 토벌_파괴된도시(boolean isNew) {
 	
 	}
-	private void 토벌_마법이깃든숲() {
+	private void 토벌_마법이깃든숲(boolean isNew) {
 	
 	}
-	private void 토벌_마계초원() {
+	private void 토벌_마계초원(boolean isNew) {
 	
 	}
 	private void 토벌_마왕성() {
@@ -83,44 +94,54 @@ public class 용사 {
 	public void 상점() {
 		System.out.println("\n상점이다. 어떤걸 구매해볼까? (잔액 : "+코인반환()+"코인)");
 		
+		아이템아이디 목록;
+		목록.표시이름반환();
 	}
 	public void 단련() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("\n단련장이다. 어떤걸 단련해볼까? (잔액 : "+코인반환()+"코인)\n1. 체력 단련\n2.공격력 단련\n3. 방어력 단련\n(모든 단련은 100코인을 소모한다.)");
-		int choice = sc.nextInt();
-		switch(choice) {
-			case 1: {
-				if (코인반환() >= 100)
-				{
-					maxhp+=5;
-					coin-=100;
-					System.out.println("체력 단련 완료\n최대체력이 5 증가했다.\n");
+		while (true) {
+			System.out.print("\n단련장이다. 어떤걸 단련해볼까? (잔액 : "+코인반환()+"코인, 모든 단련은 100코인을 소모한다.)\n1. 체력 단련\n2. 공격력 단련\n3. 방어력 단련\n4. 뒤로\n>>");
+			int choice = sc.nextInt();
+			switch(choice) {
+				case 1: {
+					if (코인반환() >= 100)
+					{
+						if(hp == maxhp)
+							hp+=5;
+						maxhp+=5;
+						coin-=100;
+						System.out.println("체력 단련 완료\n최대체력이 5 증가했다.\n");
+					}
+					else
+						System.out.println("단련하기엔 잔액이 부족하다.");
+					break;
 				}
-				else
-					System.out.println("단련하기엔 잔액이 부족하다.");
-				break;
-			}
-			case 2: {
-				if (코인반환() >= 100)
-				{
-					attack+=1;
-					coin-=100;
-					System.out.println("공격력 단련 완료\n공격력이 1 증가했다.\n");
+				case 2: {
+					if (코인반환() >= 100)
+					{
+						attack+=1;
+						coin-=100;
+						System.out.println("공격력 단련 완료\n공격력이 1 증가했다.\n");
+					}
+					else
+						System.out.println("단련하기엔 잔액이 부족하다.");
+					break;
 				}
-				else
-					System.out.println("단련하기엔 잔액이 부족하다.");
-				break;
-			}
-			case 3: {
-				if (코인반환() >= 100)
-				{
-					defense+=1;
-					coin-=100;
-					System.out.println("방어력 단련 완료\n방어력이 1 증가했다.\n");
+				case 3: {
+					if (코인반환() >= 100)
+					{
+						defense+=1;
+						coin-=100;
+						System.out.println("방어력 단련 완료\n방어력이 1 증가했다.\n");
+					}
+					else
+						System.out.println("단련하기엔 잔액이 부족하다.");
+					break;
 				}
-				else
-					System.out.println("단련하기엔 잔액이 부족하다.");
-				break;
+				case 4: {
+					System.out.println("단련장을 나왔다.");
+					return;
+				}
 			}
 		}
 	}
